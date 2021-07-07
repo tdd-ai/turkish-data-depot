86e3a5f6111df1efeb32386a236308526d2f9d4b
Enes Ozturk <enesozturk.d@gmail.com>
2021-07-07 23:59:14 +0300

    feat: add react markdown package and sample details page

--- frontend-app/turkish-data-depot-frontend/src/screens/DataSetDetails/DataSetDetails.styled.js
+++ frontend-app/turkish-data-depot-frontend/src/screens/DataSetDetails/DataSetDetails.styled.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-const DataSetDetailContainer = styled.div``;
+const DataSetDetailContainer = styled.div`
+  padding: 20px 40px;
+
+  blockquote {
+    background: #f5f5f5;
+    padding: 16px;
+    border-left: 8px solid #d2d2d2;
+  }
+
+  blockquote p {
+    margin-bottom: 0;
+  }
+`;
 
 export { DataSetDetailContainer };
--- frontend-app/turkish-data-depot-frontend/src/screens/DataSetDetails/DatasetDetails.js
+++ frontend-app/turkish-data-depot-frontend/src/screens/DataSetDetails/DatasetDetails.js
@@ -1,7 +1,39 @@
+import ReactMarkdown from "react-markdown";
 import { DataSetDetailContainer } from "./DataSetDetails.styled";
 
+const markdown = `
+  # Title
+  ## Title
+  ### Title
+  #### Title
+  ##### Title
+  ###### Title
+
+  Lorem ipsum dolor si amet.
+
+  > Lorem ipsum dolor si amet.
+
+  ![Image](https://placehold.it/500x500)
+`;
+
 const DatasetDetails = () => {
-  return <DataSetDetailContainer />;
+  return (
+    <DataSetDetailContainer>
+      <h1>Dataset Title</h1>
+      <h3>Dataset Details Catalog</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        auctor, velit eget rhoncus ultricies, nisi nulla condimentum enim, eget
+        porta lorem leo eu nunc. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Curabitur auctor, velit eget rhoncus ultricies, nisi
+        nulla condimentum enim, eget porta lorem leo eu nunc. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Curabitur auctor, velit eget
+        rhoncus ultricies, nisi nulla condimentum enim, eget porta lorem leo eu
+        nunc.
+      </p>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </DataSetDetailContainer>
+  );
 };
 
 export default DatasetDetails;
